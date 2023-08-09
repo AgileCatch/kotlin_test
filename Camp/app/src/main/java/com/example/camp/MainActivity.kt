b4225093ddbc37bcc0001df04a1a73d24b69eafd
yeonghyeon <agilecatch@gmail.com>
2023-08-09 17:17:41 +0900

    viewPager2 구현완료

--- Camp/app/src/main/java/com/example/camp/MainActivity.kt
+++ Camp/app/src/main/java/com/example/camp/MainActivity.kt
@@ -6,6 +6,11 @@ import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var adapter: FragmentPageAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,14 +19,42 @@ class MainActivity : AppCompatActivity() {
         val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val pagerAdapter = Adapter(supportFragmentManager)
+        //viewPager2&tabLayout
+        viewPager2=findViewById(R.id.viewPager2)
+        tabLayout=findViewById(R.id.tabLayout)
 
-        val viewPager2=findViewById<ViewPager2>(R.id.viewPager)
-        viewPager2.adapter=pagerAdapter
+        adapter=FragmentPageAdapter(supportFragmentManager,lifecycle)
+        tabLayout.addTab(tabLayout.newTab().setText("Todo"))
+        tabLayout.addTab(tabLayout.newTab().setText("Bokmark"))
 
-        val tabLayout=findViewById<TabLayout>(R.id.tabLayout)
-        tabLayout.setupWithViewPager(viewPager)
+        viewPager2.adapter=adapter
+
+        tabLayout.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    viewPager2.currentItem = tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
+        viewPager2.registerOnPageChangeCallback(object :ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
+
+        }
 
     }
 
-}
+
+
--- Camp/app/src/main/java/com/example/camp/TodoFragment.kt
+++ Camp/app/src/main/java/com/example/camp/TodoFragment.kt
@@ -5,9 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 
 class TodoFragment : Fragment() {
 
