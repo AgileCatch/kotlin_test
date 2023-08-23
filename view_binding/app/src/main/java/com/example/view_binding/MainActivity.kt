88af59712cbab0ac7aa6cc678f102cad0e8b93e8
yeonghyeon <agilecatch@gmail.com>
2023-08-23 15:51:08 +0900

    과제 개발중

--- view_binding/app/src/main/java/com/example/view_binding/MainActivity.kt
+++ view_binding/app/src/main/java/com/example/view_binding/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.view_binding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.view_binding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            binding.textView.text="바인딩이 잘 되네요!"
+        }
     }
 }
