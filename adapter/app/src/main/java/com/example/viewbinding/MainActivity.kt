2cab5bc40bdb18cad221f9a02cb917fa5b9b4590
yeonghyeon <agilecatch@gmail.com>
2023-08-23 20:58:24 +0900

    어댑터를 사용한 CustomView,RecyclerView
    공부중 입니다.

---
+++ adapter/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.viewbinding
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnMain.setOnClickListener {
+            binding.tvMain.text = "바인딩이 잘 되었습니다!"
+        }
+
+        binding.btnLv.setOnClickListener {
+            val view1 = Intent(this@MainActivity,ListviewActivity::class.java)
+            startActivity(view1)
+        }
+
+        binding.btnGv.setOnClickListener {
+            val view2 = Intent(this@MainActivity,ImgGridviewActivity::class.java)
+            startActivity(view2)
+        }
+
+
+    }
+}
