9ba3a03f288fe8d4aefff3f09df5703e364a923e
yeonghyeon <agilecatch@gmail.com>
2023-09-22 11:13:18 +0900

    [FEAT] : ⚙️ | api 이미지 , 비디오 불러오기

--- ImageLibrary/app/src/main/java/com/example/imagelibrary/searchresults/search/SearchListAdapter.kt
+++ ImageLibrary/app/src/main/java/com/example/imagelibrary/searchresults/search/SearchListAdapter.kt
@@ -5,6 +5,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.imagelibrary.R
 import com.example.imagelibrary.databinding.LockerItemBinding
 
 
@@ -42,7 +44,10 @@ class SearchListAdapter : ListAdapter<SearchModel, SearchListAdapter.ViewHolder>
         private val binding: LockerItemBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: SearchModel)= with(binding){
-//이미지, 추가해야함
+            Glide.with(itemView).load(item.url)
+                .placeholder(R.drawable.ic_cat) // 이미지 로딩 중 사진
+                .error(R.drawable.icon_block) // 이미지를 불러오지 못했을 때 사진
+                .into(imageView)
             tvDate.text=item.dateTime
             tvTitle.text=item.title
 
@@ -52,50 +57,3 @@ class SearchListAdapter : ListAdapter<SearchModel, SearchListAdapter.ViewHolder>
 
 
 }
-
-
-//
-//class SearchListAdapter : ListAdapter<SearchModel, SearchListAdapter.ViewHolder>(
-//    object : DiffUtil.ItemCallback<SearchModel>() {
-//        override fun areItemsTheSame(
-//            oldItem: SearchModel, newItem: SearchModel
-//        ): Boolean {
-//            return oldItem.url == newItem.url
-//        }
-//
-//        override fun areContentsTheSame(
-//            oldItem: SearchModel,
-//            newItem: SearchModel
-//        ): Boolean {
-//            return oldItem == newItem
-//        }
-//    }
-//) {
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-//        return ViewHolder(
-//            LockerItemBinding.inflate(
-//                LayoutInflater.from(parent.context)
-//            )
-//        )
-//    }
-//
-//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        val item = getItem(position)
-//        holder.bind(item)
-//    }
-//
-//    class ViewHolder(
-//        private val binding: LockerItemBinding
-//    ) : RecyclerView.ViewHolder(binding.root) {
-//        fun bind(item: SearchModel)= with(binding){
-////이미지, 추가해야함
-//            tvDate.text=item.dateTime
-//            tvTitle.text=item.title
-//
-//        }
-//
-//    }
-//
-//
-//}
